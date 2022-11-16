38d58213901a68c9cdd92f800147acf669ef6053
jonahhowell <jonahowell800@gmail.com>
2022-11-15 20:51:02 -0500

    Changed home to function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-class Home extends React.Component {
-    render() {
-        return (<h1>Connect Carolina II!</h1>);
-    }
+function Home() {
+    return (
+        <h1>ConnectCarolina II!</h1>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

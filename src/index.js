77801c9e8ce09a125d6f89886c4351e210809ff9
jonahhowell <jonahowell800@gmail.com>
2022-11-16 09:29:44 -0500

    Imported bootstrap CSS, navbar works, got UNC png

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 function Home() {
     return (
-        <h1>ConnectCarolina II!</h1>
+        <h1>Connect Carolina II!</h1>
+    );
+}
+
+function CCNavBar() {
+    return (
+        
+        <Navbar variant="dark" bg="dark">
+            <Container>
+                <Navbar.Brand>Home</Navbar.Brand>
+            </Container>
+
+        </Navbar>
+        
     );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Home />);
+root.render(<CCNavBar />);
